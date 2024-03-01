c683539c89c0f9d03d6fade534a5d3567a04a10e
enochCodes <henoksamson57@gmail.com>
2024-03-01 19:36:09 +0300

    edit_the_shell

--- shell.h
+++ shell.h
@@ -1,14 +1,28 @@
-#ifndef _SHELL_H
-#define _SHELL_H
-
+#ifndef SHELL_H
+#define SHELL_H
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <stddef.h>
-#include <sys/types.h>
-#include <string.h>
-#include <unistd.h>
-
-#define MAX_LINE_LENGTH 1024
-
+#include <stdlib.h>
+#define MAX_ARGS 100
+#include <sys/stat.h>
+char *create_filepath(const char *token, const char *args);
+size_t tokenizer(char *input, char *delim);
+char **word_list(char *input, char *delim);
+char **append(char **args, char *token);
+void teborne(char **args);
+void exiting(char **args, char *input);
+void envir(char **args);
+void printenv(void);
+char *loc(char *path, char *args);
+char *locate(char *path);
+void printprompt(void);
+char *readinput(void);
+char **tokenize(char *input);
+void executecommand(char **args, char **env);
+extern char **environ;
 #endif
-
